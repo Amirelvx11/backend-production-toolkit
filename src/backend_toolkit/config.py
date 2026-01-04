7cdb79e412392b041303b21a5b93d3344270b5d8
Amirelvx11 <jamshidiamirmahdi1@gmail.com>
2026-01-04 09:11:49 +0330

    change src/config

--- src/backend_toolkit/config.py
+++ src/backend_toolkit/config.py
@@ -1,7 +1,6 @@
 from pydantic import Field
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
-
 class Settings(BaseSettings):
     app_name: str = "backend-toolkit"
     environment: str = Field(default="local")
